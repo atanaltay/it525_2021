42e5f441614028d9f498342937f3c04beb1542eb
atanaltay <atanaltay@gmail.com>
2021-02-13 15:31:14 +0300

    fnll

--- 09_Swing2/src/flexlayout/FlexLayoutFrame.java
+++ 09_Swing2/src/flexlayout/FlexLayoutFrame.java
@@ -4,6 +4,8 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -13,6 +15,9 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.EtchedBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
@@ -28,16 +33,17 @@ public class FlexLayoutFrame extends JFrame implements ActionListener{
 	private JPanel panel;
 	private JButton btn1;
 	private JButton btn2;
-	private JComboBox comboBox;
+	private JComboBox<String> cmbCity;
 	private JPanel panel_1;
 	private JLabel lblNewLabel;
-	private JLabel lblNewLabel_1;
+	private JLabel lblOutput;
 	private JSplitPane splitPane;
 	private JScrollPane scrollPane;
 	private JScrollPane scrollPane_1;
-	private JList listMenu;
+	private JList<String> listMenu;
 	private JTextArea txtOutput;
 	private JButton btn3;
+	private JButton btn4;
 
 	/**
 	 * Launch the application.
@@ -153,8 +159,39 @@ public class FlexLayoutFrame extends JFrame implements ActionListener{
 		
 		panel.add(btn3);
 		
-		comboBox = new JComboBox();
-		panel.add(comboBox);
+		btn4 = new JButton("Button 4");
+		btn4.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				JOptionPane.showMessageDialog(FlexLayoutFrame.this, "Button 4 clickedd...");
+			}
+		});
+		panel.add(btn4);
+		
+		
+		DefaultComboBoxModel<String> cmbCityModel = new DefaultComboBoxModel<>(new String[] {"ISTANBUL","ANKARA","IZMIR"});
+		//cmbCityModel.addElement("An item");
+		
+		
+		
+		cmbCity = new JComboBox<>();
+		cmbCity.setModel(cmbCityModel);		
+		cmbCity.addItemListener(new ItemListener() {
+			
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				if(e.getStateChange()==ItemEvent.SELECTED) {
+					System.out.println("Item selected:" + cmbCity.getSelectedItem());
+					lblOutput.setText(cmbCity.getSelectedItem().toString());
+				}
+				
+				
+			}
+		});
+		
+		
+		
+		panel.add(cmbCity);
 		
 		panel_1 = new JPanel();
 		contentPane.add(panel_1, BorderLayout.SOUTH);
@@ -162,8 +199,8 @@ public class FlexLayoutFrame extends JFrame implements ActionListener{
 		lblNewLabel = new JLabel("Copyright 2021");
 		panel_1.add(lblNewLabel);
 		
-		lblNewLabel_1 = new JLabel("Output");
-		panel_1.add(lblNewLabel_1);
+		lblOutput = new JLabel("Output");
+		panel_1.add(lblOutput);
 		
 		splitPane = new JSplitPane();
 		contentPane.add(splitPane, BorderLayout.CENTER);
@@ -172,7 +209,26 @@ public class FlexLayoutFrame extends JFrame implements ActionListener{
 		scrollPane = new JScrollPane();
 		splitPane.setLeftComponent(scrollPane);
 		
-		listMenu = new JList();
+		DefaultComboBoxModel<String> lstModel = new DefaultComboBoxModel<>(new String[] {"Page 1","Page 2","Page 3"});
+		
+		listMenu = new JList<>();
+		listMenu.setModel(lstModel);
+		listMenu.addListSelectionListener(new ListSelectionListener() {
+			
+			@Override
+			public void valueChanged(ListSelectionEvent e) {
+				
+				if(e.getValueIsAdjusting()==false) {
+					System.out.println(listMenu.getSelectedValue());
+					
+					txtOutput.setText(listMenu.getSelectedValue() + "\n" + "Content of the page.....");
+					
+					
+				}
+	
+			}
+		});
+		
 		scrollPane.setViewportView(listMenu);
 		
 		scrollPane_1 = new JScrollPane();
